d16d7b38f6e891b8bd41fec8bd8322fabafcbcf3
Kai Warren <kaiwarren12@yahoo.com>
2021-02-26 13:01:21 -0800

    added package json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -11,7 +11,7 @@ dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (request, response) => {
@@ -25,13 +25,16 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.mongoURI;
-mongoose.connect(MONGODB_URI, {
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
   useCreateIndex: true,
-});
+  useFindAndModify: false
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+}).then(() => console.log("Database Connected Successfully"))
+  .catch(err => console.log(err));
+
+// Start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
