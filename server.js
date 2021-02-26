5fcfb0928547a14fc843fdfaaf249acb60daadeb
Kai Warren <kaiwarren12@yahoo.com>
2021-02-26 13:02:11 -0800

    updated server code

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (request, response) => {
